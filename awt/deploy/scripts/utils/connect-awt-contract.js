8771254815f0e43537fbafea0a579f98cb74f703
Dev43 <patrick@bitbackapp.com>
2023-01-20 11:04:44 -0500

    cleanup

--- awt/deploy/scripts/utils/connect-awt-contract.js
+++ awt/deploy/scripts/utils/connect-awt-contract.js
@@ -1,6 +1,5 @@
 const { getWarpInstance } = require('../deploy');
 
-// import { AwtContract } from '../../../contract/definition/bindings/ts/AwtContract';
 module.exports.connectAWTContract = async function (arweave, wallet, contractTxId, target) {
   const { AwtContract } = require('../../../contract/definition/bindings/ts/AwtContract');
   const warp = getWarpInstance(arweave.api.config.port, target);
